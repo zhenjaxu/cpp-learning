f12e44bd2d7c039f0aa4d86d23999e3b7c07fd84
Don-qq <2263535378@qq.com>
2026-05-22 20:08:25 +0800

    新增游戏编程SDL2安装与测试

---
+++ Game/CppGameDemo/SDL2_test/build/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/main.cpp.obj"
+  "CMakeFiles/game.dir/main.cpp.obj.d"
+  "game.exe"
+  "game.exe.manifest"
+  "game.pdb"
+  "libgame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Game/README.md
+++ Game/README.md
@@ -3,6 +3,7 @@
 ## 书籍学习
 
 1. Game Programming Patterns
+2. Game Programming in C++
 
 ## 项目尝试
 

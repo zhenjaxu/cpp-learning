327833a489c6c97c7e80b4405adde6652f3c13a8
Don-qq <2263535378@qq.com>
2026-05-29 23:50:38 +0800

    Pong新增双人模式

--- Game/Programming/Game/Pong/Game.hpp
+++ Game/Programming/Game/Pong/Game.hpp
@@ -70,10 +70,12 @@ private:
     const int thickness=15;
     const int paddleH=100;
     Position mBallPos{512, 384};
-    Position mPaddlePos{20, 384};
+    Position mPaddle1Pos{20, 384};
+    Position mPaddle2Pos{1004, 384};
 
-    Position mBallVel{-400.0f, 435.0f};
-    int mPaddleDir;
+    Position mBallVel{-200.0f, 235.0f};
+    int mPaddle1Dir;
+    int mPaddle2Dir;
 
     Uint32 mTicksCount;
 };
@@ -85,7 +87,6 @@ void Game::GenerateOutput(){
     SDL_SetRenderDrawColor(mRenderer, 255, 255, 255, 255);
 
     SDL_Rect wall1{0, 0, 1024, thickness};
-    SDL_Rect wall2{1024-thickness, 0, thickness, 768};
     SDL_Rect wall3{0, 768-thickness, 1024, thickness};
 
     SDL_Rect ball{
@@ -94,18 +95,24 @@ void Game::GenerateOutput(){
         thickness,
         thickness
     };
-    SDL_Rect paddle{
-        static_cast<int>(mPaddlePos.x-thickness/2),
-        static_cast<int>(mPaddlePos.y-paddleH/2),
+    SDL_Rect paddle1{
+        static_cast<int>(mPaddle1Pos.x-thickness/2),
+        static_cast<int>(mPaddle1Pos.y-paddleH/2),
+        thickness,
+        paddleH
+    };
+    SDL_Rect paddle2{
+        static_cast<int>(mPaddle2Pos.x-thickness/2),
+        static_cast<int>(mPaddle2Pos.y-paddleH/2),
         thickness,
         paddleH
     };
 
     SDL_RenderFillRect(mRenderer, &wall1);
-    SDL_RenderFillRect(mRenderer, &wall2);
     SDL_RenderFillRect(mRenderer, &wall3);
     SDL_RenderFillRect(mRenderer, &ball);
-    SDL_RenderFillRect(mRenderer, &paddle);
+    SDL_RenderFillRect(mRenderer, &paddle1);
+    SDL_RenderFillRect(mRenderer, &paddle2);
     
     SDL_RenderPresent(mRenderer);
 }
@@ -128,12 +135,19 @@ void Game::ProcessInput(){
         mIsRunning=false;
     }
     
-    mPaddleDir=0;
+    mPaddle1Dir=0;
+    mPaddle2Dir=0;
     if(state[SDL_SCANCODE_W]){
-        mPaddleDir-=1;
+        mPaddle1Dir-=1;
     }
     if(state[SDL_SCANCODE_S]){
-        mPaddleDir+=1;
+        mPaddle1Dir+=1;
+    }
+    if(state[SDL_SCANCODE_I]){
+        mPaddle2Dir-=1;
+    }
+    if(state[SDL_SCANCODE_K]){
+        mPaddle2Dir+=1;
     }
 }
 
@@ -150,30 +164,43 @@ void Game::UpdateGame(){
        mBallPos.y>(768.0f-thickness)&&mBallVel.y>0.0f){
         mBallVel.y*=-1;
     }
-    if(mBallPos.x>(1024.0f-thickness)&&mBallVel.x>0.0f){
-        mBallVel.x*=-1;
-    }
 
-    if(std::abs(mBallPos.y-mPaddlePos.y)<paddleH/2.0f&&
+    if(std::abs(mBallPos.y-mPaddle1Pos.y)<paddleH/2.0f&&
        mBallPos.x<35.0f&&mBallPos.x>12.5f&&
        mBallVel.x<0.0f){
         mBallVel.x*=-1;
     }
 
-    if(mPaddleDir!=0){
-        mPaddlePos.y+=mPaddleDir*300.0f*deltaTime;
+    if(std::abs(mBallPos.y-mPaddle2Pos.y)<paddleH/2.0f&&
+       mBallPos.x<1011.5f&&mBallPos.x>989.0f&&
+       mBallVel.x>0.0f){
+        mBallVel.x*=-1;
+    }
 
-        if(mPaddlePos.y<(paddleH/2.0f+thickness)){
-            mPaddlePos.y=paddleH/2.0f+thickness;
-        }else if(mPaddlePos.y>(768.0f-paddleH/2.0f-thickness)){
-            mPaddlePos.y=768.0f-paddleH/2.0f-thickness;
+    if(mPaddle1Dir!=0){
+        mPaddle1Pos.y+=mPaddle1Dir*300.0f*deltaTime;
+
+        if(mPaddle1Pos.y<(paddleH/2.0f+thickness)){
+            mPaddle1Pos.y=paddleH/2.0f+thickness;
+        }else if(mPaddle1Pos.y>(768.0f-paddleH/2.0f-thickness)){
+            mPaddle1Pos.y=768.0f-paddleH/2.0f-thickness;
+        }
+    }
+
+    if(mPaddle2Dir!=0){
+        mPaddle2Pos.y+=mPaddle2Dir*300.0f*deltaTime;
+
+        if(mPaddle2Pos.y<(paddleH/2.0f+thickness)){
+            mPaddle2Pos.y=paddleH/2.0f+thickness;
+        }else if(mPaddle2Pos.y>(768.0f-paddleH/2.0f-thickness)){
+            mPaddle2Pos.y=768.0f-paddleH/2.0f-thickness;
         }
     }
 
     mBallPos.x+=mBallVel.x*deltaTime;
     mBallPos.y+=mBallVel.y*deltaTime;
 
-    if(mBallPos.x<0){
+    if(mBallPos.x<0||mBallPos.x>1024){
         mBallPos.x=512;
         mBallPos.y=384;
         mBallVel.x*=-1;

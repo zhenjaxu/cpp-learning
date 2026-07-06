b0ac9e7c50614a42130414338a04f38697772dbe
Don-qq <2263535378@qq.com>
2026-07-06 17:31:11 +0800

    新增游戏学习4.AI塔防项目CircleComponent类实现

---
+++ Game/Programming/AI/Src/Engine/Core/CircleComponent.cpp
@@ -0,0 +1,25 @@
+#include"CircleComponent.h"
+#include"Actor.h"
+
+CircleComponent::CircleComponent(class Actor* owner, int updateOrder)
+:Component(owner, updateOrder)
+,mRadius(0.0f)
+{}
+
+float CircleComponent::GetRadius() const {
+    return mOwner->GetScale()*mRadius;
+}
+
+const Vector2& CircleComponent::GetCenter() const {
+    return mOwner->GetPosition();
+}
+
+bool Intersect(const CircleComponent* a, const CircleComponent* b){
+    Vector2 diff=a->GetCenter()-b->GetCenter();
+    float diffSq=diff.LengthSq();
+
+    float radiiSq=a->GetRadius()+b->GetRadius();
+    radiiSq*=radiiSq;
+
+    return diffSq<=radiiSq;
+}
